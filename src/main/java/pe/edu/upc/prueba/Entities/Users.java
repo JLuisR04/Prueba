1e6e145b06e91b45409ee4fdf83b6ecaf3be3716
JLuisR04 <u202110143@upc.edu.pe>
2024-04-19 15:01:57 -0500

    he agregado un nuevo package "Entities" y además en ese package he agregado nuestra tercera entidad llamada "Users"

---
+++ src/main/java/pe/edu/upc/prueba/Entities/Users.java
@@ -0,0 +1,51 @@
+package pe.edu.upc.prueba.Entities;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, unique = true)
+    private String username;
+    @Column(length = 200)
+    private String password;
+    private Boolean enabled;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
